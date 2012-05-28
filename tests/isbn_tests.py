98461cf02c6b3ec94dc7f0d090963b5c9f75bcb4
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-05-28 14:45:52 -0700

    Testing now works!!!! It's really cool.

--- tests/isbn_tests.py
+++ tests/isbn_tests.py
@@ -1,8 +1,34 @@
+import sys
+sys.path.append('/home/luis/Dropbox/projects/android/isbn')
 from isbn.isbn import Book
 
-def test_all():
-    isbn = '0312538618'
-    book = Book(isbn)
-    assert book.isbn ==  isbn
+tests = {
+    '9780312538613': {  # ISBN at __init__
+        'isbn10': '0312538618',  # These should all be expected attributes of the new instance
+        'isbn13': '9780312538613',
+        'title': "Everything's an argument : with readings",
+        'author': "Andrea A. Lunsford, John J. Ruszkiewicz, Keith Walters.",
+        'publisher': "Bedford/St. Martins",
+        'year': '2010',
+        'city': 'Boston',
 
+    },
+}
+
+def test_book(isbn, info):
+    book = Book(isbn)
+    assert book.isbn == isbn
+
+    book.getMetadata()
+    book.to10()
+    book.to13()
+
+    for attribute in info:
+        assert getattr(book, attribute) == info[attribute]
+
+
+def test_all():
+    for isbn in tests:
+        info = tests[isbn]
+        test_book(isbn, info)
 test_all()
